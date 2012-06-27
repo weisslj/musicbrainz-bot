8f24822a921ad8369a7ace8db03a709ca63b875f
Aurélien Mino <aurelien.mino@gmail.com>
2012-06-27 22:48:03 +0200

    A few changes to cover art: renamed script, make it possible to call the script for a MBID that doesn't have a discogs link, improved FR filter and obscured edit note to notice source for Itunes and Spotify images

--- discogs_cover_art.py
+++ discogs_cover_art.py
@@ -21,7 +21,7 @@ from mbbot.source.itunes import ItunesSearchAPI
 
 engine = sqlalchemy.create_engine(cfg.MB_DB)
 db = engine.connect()
-db.execute("SET search_path TO musicbrainz")
+db.execute("SET search_path TO musicbrainz, mbbot")
 
 mb = MusicBrainzClient(cfg_caa.MB_USERNAME, cfg_caa.MB_PASSWORD, cfg_caa.MB_SITE)
 
@@ -59,25 +59,28 @@ WITH
         SELECT r.id, discogs_url.url as discogs_url, amz_url.url AS amz_url
         FROM release r
             JOIN release_meta rm ON rm.id = r.id
-            JOIN l_release_url discogs_link ON discogs_link.entity0 = r.id AND discogs_link.link IN (SELECT id FROM link WHERE link_type = 76)
+            LEFT JOIN l_release_url discogs_link ON discogs_link.entity0 = r.id AND discogs_link.link IN (SELECT id FROM link WHERE link_type = 76)
                 AND discogs_link.edits_pending = 0
-            JOIN url discogs_url ON discogs_url.id = discogs_link.entity1
+            LEFT JOIN url discogs_url ON discogs_url.id = discogs_link.entity1
             LEFT JOIN l_release_url amz_link ON amz_link.entity0 = r.id AND amz_link.link IN (SELECT id FROM link WHERE link_type = 77)
                 AND amz_link.edits_pending = 0
             LEFT JOIN url amz_url ON amz_url.id = amz_link.entity1
             LEFT JOIN release_status rs ON r.status = rs.id
-            JOIN (SELECT DISTINCT acn.artist_credit
-                FROM artist_credit_name acn
-                JOIN artist a ON acn.artist = a.id
-                JOIN country c ON a.country = c.id AND (c.iso_code = 'FR' OR a.id = 1)
-            ) tc ON r.artist_credit = tc.artist_credit
-            JOIN country rc ON rc.id = r.country AND rc.iso_code = 'FR'
-            LEFT JOIN (SELECT encycl_link.entity0, encycl_url.url
+            LEFT JOIN country rc ON rc.id = r.country
+            LEFT JOIN (SELECT encycl_link.entity0, encycl_link.entity1, encycl_url.url
                 FROM l_release_url encycl_link
                 JOIN url encycl_url ON encycl_url.id = encycl_link.entity1 AND encycl_url.url ~ 'encyclopedisque.fr/images/'
                 WHERE encycl_link.link IN (SELECT id FROM link WHERE link_type = 78) AND encycl_link.edits_pending = 0
             ) encycl_link ON encycl_link.entity0 = r.id
         WHERE """ + filter_clause + """
+            /* Artist is either French or Various Artists. Pick only French Various Artists releases */
+            AND EXISTS (SELECT 1
+                FROM artist_credit_name acn
+                JOIN artist a ON acn.artist = a.id
+                JOIN country c ON a.country = c.id
+                WHERE r.artist_credit = acn.artist_credit
+                    AND (c.iso_code = 'FR' OR a.id = 1) AND (a.id <> 1 OR rc.iso_code = 'FR')
+            )
             /* Discogs link should only be linked to this release */
             AND NOT EXISTS (SELECT 1 FROM l_release_url l WHERE l.entity1 = discogs_url.id AND l.entity0 <> r.id)
             /* this release should not have another Discogs link attached */
@@ -88,6 +91,8 @@ WITH
             /* this release should not have another Amazon link attached */
             AND NOT EXISTS (SELECT 1 FROM l_release_url l WHERE l.entity0 = r.id AND l.entity1 <> amz_url.id
                                 AND l.link IN (SELECT id FROM link WHERE link_type = 77))
+            /* Encylopedisque link should only be linked to this release */
+            AND NOT EXISTS (SELECT 1 FROM l_release_url l WHERE l.entity1 = encycl_link.entity1 AND l.entity0 <> r.id)
             /* Promotion or Bootleg OR release_year < 1980 OR barcode and Amazon => OK */
             AND (rs.name IN ('Promotion','Bootleg')
                 OR date_year < 1980
@@ -95,6 +100,8 @@ WITH
                 OR encycl_link.url IS NOT NULL
                 """ + ("OR TRUE" if mbid else "") + """
             )
+            /* Discogs URL required, unless an explicit MBID is provided */
+            AND (discogs_url.url IS NOT NULL """ + ("OR TRUE" if mbid else "") + """)
     )
 SELECT r.id, r.gid, r.name, tr.discogs_url, tr.amz_url, ac.name AS artist, r.barcode, b.processed
 FROM releases_wo_coverart tr
@@ -175,19 +182,20 @@ def already_processed(release, url):
     res = db.execute("SELECT 1 FROM bot_release_artwork_url WHERE release = %s AND url = %s", (release, url))
     return res.scalar() is not None
 
-def submit_cover_art(release, url, types):
+def submit_cover_art(release, url, types, editnote=u''):
     if already_processed(release, url):
         colored_out(bcolors.NONE, " * skipping already submitted image '%s'" % (url,))
     else:
         colored_out(bcolors.OKGREEN, " * Adding " + ",".join(types) + (" " if len(types)>0 else "") + "cover art '%s'" % (url,))
         time.sleep(5)
         #mb.add_cover_art(release, url, types)
-        mb.add_cover_art(release, url, types, None, u'', u'', False, True)
+        mb.add_cover_art(release, url, types, None, u'', editnote, False, True)
         save_processed(release, url)
 
 for release in db.execute(query):
     colored_out(bcolors.OKBLUE, 'Examining release "%s" by "%s" http://musicbrainz.org/release/%s' % (release['name'], release['artist'], release['gid']))
 
+    editnote=u''
     # Front cover
     # Start with Discogs if available
     colored_out(bcolors.HEADER, ' * Discogs = %s' % (release['discogs_url'],))
@@ -230,6 +238,7 @@ for release in db.execute(query):
                 if spotify_score >= best_score:
                     front_uri = image_url
                     best_score = spotify_score
+                    editnote = u'TQ'
 
     # Evaluate iTunes
     if release['barcode'] is not None and release['barcode'] != "":
@@ -244,9 +253,10 @@ for release in db.execute(query):
             if itunes_score >= best_score:
                 front_uri = image_url
                 best_score = itunes_score
+                editnote = u'JU'
 
     if front_uri is not None:
-        submit_cover_art(release['gid'], front_uri, ['front'])
+        submit_cover_art(release['gid'], front_uri, ['front'], editnote)
 
     # Other images
     for image in discogs_get_secondary_images(release['discogs_url']):
