194955185af5888153a85d72b1181153e65cc0aa
Marti Raudsepp <marti@juffo.org>
2013-03-03 19:00:38 +0200

    Include monkey-patch for mechanize bug
    More user-friendly than requiring the user to manually install the
    patched version.

--- utils.py
+++ utils.py
@@ -237,3 +237,25 @@ def structureToString(obj):
         for key in sorted(obj.iterkeys()):
             ret.append("%s:%s" % ( key, structureToString(obj[key]) ))
         return '{' + ",".join(ret) + '}'
+
+def monkeypatch_mechanize():
+    """Work-around for a mechanize 0.2.5 bug. See: https://github.com/jjlee/mechanize/pull/58"""
+    import mechanize
+    if mechanize.__version__ < (0, 2, 6):
+        from mechanize._form import SubmitControl, ScalarControl
+
+        def __init__(self, type, name, attrs, index=None):
+            ScalarControl.__init__(self, type, name, attrs, index)
+            # IE5 defaults SUBMIT value to "Submit Query"; Firebird 0.6 leaves it
+            # blank, Konqueror 3.1 defaults to "Submit".  HTML spec. doesn't seem
+            # to define this.
+            if self.value is None:
+                if self.disabled:
+                    self.disabled = False
+                    self.value = ""
+                    self.disabled = True
+                else:
+                    self.value = ""
+            self.readonly = True
+
+        SubmitControl.__init__ = __init__
